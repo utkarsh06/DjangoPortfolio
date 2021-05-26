c7acbf892a7cf533c16b3c91bc2b5dbe24c8ef7b
utkarsh06 <utkarsh.gaur06@gmail.com>
2021-05-26 00:32:13 -0400

    Created models for jobs

--- Project/UtkarshGaur/models.py
+++ Project/UtkarshGaur/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
+
+class Job(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    summary = models.CharField(max_length=400)
--- Project/UtkarshGaur/views.py
+++ Project/UtkarshGaur/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Job
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'UtkarshGaur/Home.html')
+    jobs = Job.objects
+    return render(request, 'UtkarshGaur/Home.html', {'jobs':jobs})
 
 def about(request):
     return render(request, 'UtkarshGaur/About.html' )
