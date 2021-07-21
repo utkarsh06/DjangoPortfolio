e9b5bc2a78f92acf1673df07025de3d491571c34
utkarsh06 <utkarsh.gaur06@gmail.com>
2021-07-20 23:52:32 -0400

    Added files for production

--- Project/Portfolio/settings.py
+++ Project/Portfolio/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['safe-thicket-29435.herokuapp.com']
 
 
 # Application definition
