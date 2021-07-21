4db11356134a8b5e1af11470b0d76ed3d54a51e8
utkarsh06 <utkarsh.gaur06@gmail.com>
2021-07-20 23:21:10 -0400

    Updated django secret key

--- Project/Portfolio/settings.py
+++ Project/Portfolio/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dt%c5#bab@&%by+qg-3ht!bl*23m6gul1#8#a6ob%jth@rr3ol'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
